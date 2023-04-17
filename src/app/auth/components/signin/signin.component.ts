f058f8f33ebbb931e4459a855021e698ed4f9a00
unknown <ahmed.ayman16899@gmail.com>
2023-04-17 20:34:02 +0200

    add auth style

--- src/app/auth/components/signin/signin.component.ts
+++ src/app/auth/components/signin/signin.component.ts
@@ -16,8 +16,8 @@ export class SigninComponent implements OnInit {
   ngOnInit(): void {
   }
   loginForm:FormGroup=new FormGroup({
-    credential: new FormControl(null,/*[Validators.required , Validators.pattern(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)]*/),
-    password: new FormControl(null,/*[Validators.required , Validators.minLength(6)]*/),
+    credential: new FormControl(null,[Validators.required , /*Validators.pattern(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)*/]),
+    password: new FormControl(null,[Validators.required , Validators.minLength(6)]),
   })
   submitForm()
   {
