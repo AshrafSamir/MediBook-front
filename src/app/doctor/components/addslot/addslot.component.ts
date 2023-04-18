55e18abf00cac60706a56eb15b007d27761aa72a
unknown <ahmed.ayman16899@gmail.com>
2023-04-18 16:09:27 +0200

    create

---
+++ src/app/doctor/components/addslot/addslot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addslot',
+  templateUrl: './addslot.component.html',
+  styleUrls: ['./addslot.component.scss']
+})
+export class AddslotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
