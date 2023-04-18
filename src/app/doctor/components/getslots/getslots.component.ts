c93a65c65fd5a4491456f1c35ee90cb247992da6
unknown <ahmed.ayman16899@gmail.com>
2023-04-18 16:17:36 +0200

    get slots

---
+++ src/app/doctor/components/getslots/getslots.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-getslots',
+  templateUrl: './getslots.component.html',
+  styleUrls: ['./getslots.component.scss']
+})
+export class GetslotsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
