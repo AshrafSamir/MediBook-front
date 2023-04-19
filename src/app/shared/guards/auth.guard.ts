9a7ce8281e1b0645a726632f51dc7f94c49a97d4
unknown <ahmed.ayman16899@gmail.com>
2023-04-19 06:38:46 +0200

    add guard to sign components

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -12,16 +12,21 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      /*if(this._AuthService.userData.getValue() != "null")
+      if(this._AuthService.userType.getValue() == "null")
       {
         return true;
       }
+      else if (this._AuthService.userType.getValue() == "doctor")
+      {
+        this._Router.navigate(['doctor/addslot'])
+        return false;
+      }
       else
       {
-        this._Router.navigate(['login'])
+        this._Router.navigate(['patient'])
         return false;
-      }*/
-      return true
+      }
+      //return true
   }
   
 }
