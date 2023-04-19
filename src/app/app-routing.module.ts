b5e485328b2edcc4a5d0bed5032cdcd57f48319f
fayrouzhussien21 <90986446+fayrouzhussien21@users.noreply.github.com>
2023-04-19 17:33:16 +0200

    merging home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DoctorGuard } from './shared/guards/doctor.guard';
 import { PatientGuard } from './shared/guards/patient.guard';
-<<<<<<< HEAD
 import {BookingComponent} from './components/booking/booking.component';
-=======
 import { AppointmentComponent } from './components/appointment/appointment.component';
->>>>>>> 018d8dee2fae56545eef40d33e27c10c0cecf34d
 
 const routes: Routes = [
   { path: 'home', component :HomeComponent },
@@ -15,11 +12,8 @@ const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'doctor',canActivate:[DoctorGuard], loadChildren: () => import('./doctor/doctor.module').then(m => m.DoctorModule) },
   { path: 'patient',canActivate:[PatientGuard], loadChildren: () => import('./patient/patient.module').then(m => m.PatientModule) },
-<<<<<<< HEAD
-  {path: 'booking',component:BookingComponent}
-=======
+  {path: 'booking',component:BookingComponent},
   { path: 'appointment',component:AppointmentComponent }
->>>>>>> 018d8dee2fae56545eef40d33e27c10c0cecf34d
 ];
 
 @NgModule({
