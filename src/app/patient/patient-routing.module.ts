9d8f8c07271c4419643ba48412fdf0b76993af44
unknown <ahmed.ayman16899@gmail.com>
2023-04-28 12:39:20 +0300

    grapgh and loading

--- src/app/patient/patient-routing.module.ts
+++ src/app/patient/patient-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PatientComponent } from './patient.component';
+import { PatientdashboardComponent } from './components/patientdashboard/patientdashboard.component';
 
-const routes: Routes = [{ path: '', component: PatientComponent }];
+const routes: Routes = [{ path: '', component: PatientComponent },
+{ path: 'dashboard', component: PatientdashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
