50eaf0179b7c911bb976443abf786783ee9417c3
unknown <ahmed.ayman16899@gmail.com>
2023-04-29 19:59:35 +0300

    .

---
+++ src/app/patient/service/patientservice.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../api'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientserviceService {
+  id = JSON.parse(localStorage.getItem('userData'))._id
+  constructor(private _HttpClient: HttpClient, private _Router: Router) {
+  }
+  getUserDoctorFrequency(): Observable<any> {
+    return this._HttpClient.get(`${environment.ApiUrl}/userDoctorsFrequency/${this.id}`);
+  }
+  getUserDepartmentFrequency(): Observable<any> {
+    return this._HttpClient.get(`${environment.ApiUrl}/userDepartmentFrequency/${this.id}`);
+  }
+}
+
