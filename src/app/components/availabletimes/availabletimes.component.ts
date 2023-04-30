73c465e8badd99033f90ca5e46cdbc5536ce8b2d
unknown <ahmed.ayman16899@gmail.com>
2023-04-30 15:31:59 +0300

    pagination

--- src/app/components/availabletimes/availabletimes.component.ts
+++ src/app/components/availabletimes/availabletimes.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute,Router } from '@angular/router';
-import {map} from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 import { AuthService } from 'src/app/shared/services/auth.service';
 @Component({
   selector: 'app-availabletimes',
@@ -9,64 +9,80 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./availabletimes.component.scss']
 })
 export class AvailabletimesComponent implements OnInit {
-  isLoaded:boolean;
- dayname:any;
- month:any;
-    year:any;
-  doctorId:any;
-  DoctorsTimeSlots:any;
-  constructor(private route:ActivatedRoute , private http:HttpClient,private router:Router , private _auth : AuthService) { }
-  type:string = ''
+  isLoaded: boolean;
+  dayname: any;
+  month: any;
+  year: any;
+  doctorId: any;
+  DoctorsTimeSlots: any;
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router, private _auth: AuthService) { }
+  type: string = ''
   ngOnInit(): void {
-    this.isLoaded=false;
+    this.isLoaded = false;
 
-    this._auth.userType.subscribe((res)=>{
-      
-      this.type = res ;    
+    this._auth.userType.subscribe((res) => {
+
+      this.type = res;
     })
 
 
-    this.route.queryParams.subscribe(params =>{
-      this.doctorId=params['id'];
+    this.route.queryParams.subscribe(params => {
+      this.doctorId = params['id'];
       this.getAllTimeSlots();
     });
   }
-  getAllTimeSlots(){
+  getAllTimeSlots() {
     console.log(this.doctorId)
-    this.http.get(`http://localhost:3000/doctorTimeSlots/${this.doctorId}`).pipe(map((res)=>{
-      this.DoctorsTimeSlots=[];
-    for(let key in res)
-    {
-      let {from,to,fullyBooked,_id}=res[key];
-      to=new Date(to);
-      from=new Date(from);
-      this.dayname=from.toLocaleString('en-US',{weekday:'long'});
-      this.month=from.toLocaleString('en-US',{month:'numeric'});
-      this.year=from.toLocaleString('en-US',{year:'numeric'});
-      let date=`${this.dayname}.${this.month}.${this.year}`;
-      from=from.toLocaleTimeString();
-      
-      to=to.toLocaleTimeString();
-      this.DoctorsTimeSlots.push({date,from,to,fullyBooked,_id});
-    }
+    this.http.get(`http://localhost:3000/doctorTimeSlots/${this.doctorId}`).pipe(map((res) => {
+      this.DoctorsTimeSlots = [];
+      for (let key in res) {
+        let { from, to, fullyBooked, _id } = res[key];
+        to = new Date(to);
+        from = new Date(from);
+        this.dayname = from.toLocaleString('en-US', { weekday: 'long' });
+        this.month = from.toLocaleString('en-US', { month: 'numeric' });
+        this.year = from.toLocaleString('en-US', { year: 'numeric' });
+        let date = `${this.dayname}.${this.month}.${this.year}`;
+        from = from.toLocaleTimeString();
+
+        to = to.toLocaleTimeString();
+        this.DoctorsTimeSlots.push({ date, from, to, fullyBooked, _id });
+      }
+      //this.DoctorsTimeSlots.sort((a, b) => (a.date < b.date) ? 1 : (a.date === b.date) ? ((a.from < b.from) ? 1 : -1) : -1 )
+      console.log(this.DoctorsTimeSlots)
+      this.length = Array(Math.ceil(this.DoctorsTimeSlots.length / 4)).fill(0).map((_, i) => i + 1);
+      this.DoctorsTimeSlots.reverse();
       return this.DoctorsTimeSlots;
-    })).subscribe((res)=>{
-      this.isLoaded=true;
-        });
+    })).subscribe((res) => {
+      this.isLoaded = true;
+    });
   }
 
-  book(id:any)
-  {
-    if(this.type == 'null')
-    {
+  book(id: any) {
+    if (this.type == 'null') {
       this.router.navigate(['auth/signin']);
 
     }
-    else
-    {
-      this.router.navigate(['/booking'],{queryParams:{id}});
+    else {
+      this.router.navigate(['/booking'], { queryParams: { id } });
 
     }
-   
+
+  }
+
+
+
+
+  length: number[] = []
+
+  currentPage: number = 1;
+  pageSize: number = 4;
+  totalItems: number;
+  get pagedItems() {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    return this.DoctorsTimeSlots.slice(startIndex, startIndex + this.pageSize);
+  }
+  setPage(pageNumber: number) {
+    this.currentPage = pageNumber;
   }
 }
--- src/app/components/selecteddoctors/selecteddoctors.component.ts
+++ src/app/components/selecteddoctors/selecteddoctors.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-selecteddoctors',
   templateUrl: './selecteddoctors.component.html',
@@ -9,69 +9,81 @@ import {map} from 'rxjs/operators';
 })
 export class SelecteddoctorsComponent implements OnInit {
 
-  department:string;
-  address:string;
-  isLoaded:boolean;
-  doctors:string;
-  doctorid:any;
-  Doctorsinformations=[];
-  checkLoader:boolean=false
-    constructor(private route:ActivatedRoute , private http:HttpClient,private router:Router) { }
-  
-    ngOnInit(): void {
-      
-      this.route.queryParams.subscribe(params =>{
-        this.department=params['department']?params['department']:null;
-  this.address=params['address']?params['address']:null;
-  this.doctors=params['doctor']?params['doctor']:null;
-  this.postAppointment({doctors:this.doctors,address:this.address,department:this.department});
-      })
-    }
-  
-    postAppointment(appointment:{department:string,doctors:string,address:string}){
-      this.isLoaded=false;
-      // http://localhost:3000/doctorTimeSlots/
-      const httpOptions = {
-        headers: new HttpHeaders({      
-          'auth':localStorage.getItem('token')
-        })
-      };
-      this.http.get('http://localhost:3000/alldoctors').pipe(map((res)=>{
-        this.Doctorsinformations=[];
-        for(const key in res)
-        {
-          if(key=='allDoctorsData')
-          {
-            for(const key2 in res[key])
-            {
-              if(res[key][key2].specification==appointment.department||res[key][key2].clinicAddress
-                ==appointment.address||this.doctors==res[key][key2].name)
-                {
-                  
-                  this.Doctorsinformations.push(res[key][key2]);
-                }
-            }
-            if(this.Doctorsinformations.length==0)
-            {
-              this.Doctorsinformations.push(...res[key]);
-            }
-            break;
-          }
-          
-        }
-        return this.Doctorsinformations;  
-      })).subscribe((res)=>{
-        this.isLoaded=true;
-          });
-        }
-    
+  department: string;
+  address: string;
+  isLoaded: boolean;
+  doctors: string;
+  doctorid: any;
+  Doctorsinformations = [];
+  checkLoader: boolean = false
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) { }
 
-        showTimeSlots(id:any)
-        {
-this.doctorid=id;
-this.router.navigate(['/Availabletimes'],{queryParams:{id:this.doctorid}});
-   
+  ngOnInit(): void {
+
+    this.route.queryParams.subscribe(params => {
+      this.department = params['department'] ? params['department'] : null;
+      this.address = params['address'] ? params['address'] : null;
+      this.doctors = params['doctor'] ? params['doctor'] : null;
+      this.postAppointment({ doctors: this.doctors, address: this.address, department: this.department });
+    })
+  }
+
+  postAppointment(appointment: { department: string, doctors: string, address: string }) {
+    this.isLoaded = false;
+    // http://localhost:3000/doctorTimeSlots/
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'auth': localStorage.getItem('token')
+      })
+    };
+    this.http.get('http://localhost:3000/alldoctors').pipe(map((res) => {
+      this.Doctorsinformations = [];
+      for (const key in res) {
+        if (key == 'allDoctorsData') {
+          for (const key2 in res[key]) {
+            if (res[key][key2].specification == appointment.department || res[key][key2].clinicAddress
+              == appointment.address || this.doctors == res[key][key2].name) {
+
+              this.Doctorsinformations.push(res[key][key2]);
+            }
+          }
+          if (this.Doctorsinformations.length == 0) {
+            this.Doctorsinformations.push(...res[key]);
+          }
+          break;
         }
+
+      }
+      console.log(this.Doctorsinformations)
+      this.length = Array(Math.ceil(this.Doctorsinformations.length/4)).fill(0).map((_, i) => i+1);
+
+      return this.Doctorsinformations;
+    })).subscribe((res) => {
+      this.isLoaded = true;
+    });
+  }
+
+
+  showTimeSlots(id: any) {
+    this.doctorid = id;
+    this.router.navigate(['/Availabletimes'], { queryParams: { id: this.doctorid } });
+
+  }
+
+
+
+  length:number[]=[]
+
+  currentPage: number = 1;
+pageSize: number = 4;
+totalItems: number;
+get pagedItems() {
+  const startIndex = (this.currentPage - 1) * this.pageSize;
+  return this.Doctorsinformations.slice(startIndex, startIndex + this.pageSize);
+}
+setPage(pageNumber: number) {
+  this.currentPage = pageNumber;
+}
 
 }
 
